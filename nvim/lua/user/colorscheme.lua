ce69b24cf0870cc197571379d4c5216bba23bdb0
Darin Wilson <darinwilson@gmail.com>
2022-10-25 09:51:55 -0700

    Replace vanilla nvim config with astrovim (for neovim 0.7)

--- nvim/lua/user/colorscheme.lua
+++
@@ -1,8 +0,0 @@
--- local colorscheme = "darkplus"
--- DMWHACK
-local colorscheme = "oceanicnext"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  return
-end
